8cf811526edbd8741728aa7291202d8d2d3450c5
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-02-28 09:12:19 +0700

    updated user models

--- entitas/user/models.py
+++ entitas/user/models.py
@@ -1,58 +1,58 @@
 class User:
     def __init__(
-        self,
-        id=0,
-        email="",
-        password="",
-        name="",
-        token="",
-        hp="",
-        active=False,
-        last_login=None,
-        role='',
-        address="",
-        picture="",
-        created_date=None,
-        updated_date=None,
-        birth_date=None,
-        birth_place='',
-        firebase_token='',
-        ws_id=''
+            self,
+            id=0,
+            name='',
+            email='',
+            password='',
+            address='',
+            birth_date='',
+            role='',
+            birth_place='',
+            picture='',
+            firebase_token='',
+            ws_id=0,
+            activate='',
+            last_login='',
+            hp='',
+            token='',
+            created_date=None,
+            updated_date=None,
     ):
         self.id = id
+        self.name = name
         self.email = email
         self.password = password
-        self.name = name
-        self.token = token
-        self.last_login = last_login
-        self.hp = hp
-        self.picture = picture
-        self.active = active
-        self.role = role
         self.address = address
         self.birth_date = birth_date
+        self.role = role
         self.birth_place = birth_place
+        self.picture = picture
         self.firebase_token = firebase_token
         self.ws_id = ws_id
+        self.activate = activate
+        self.last_login = last_login
+        self.hp = hp
+        self.token = token
         self.created_date = created_date
         self.updated_date = updated_date
 
     def to_json(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             "password": self.password,
-            "name": self.name,
-            "token": self.token,
-            "last_login": str(self.last_login) if self.last_login is not None else None,
-            "hp": self.hp,
-            "picture": self.picture,
-            "active": self.active,
+            "address": self.address,
+            "birth_date": self.birth_date,
             "role": self.role,
-            "birth_date": str(self.birth_date) if self.birth_date is not None else None,
             "birth_place": self.birth_place,
+            "picture": self.picture,
             "firebase_token": self.firebase_token,
             "ws_id": self.ws_id,
+            "activate": self.activate,
+            "last_login": self.last_login,
+            "token": self.token,
             "created_date": str(self.created_date) if self.created_date is not None else None,
             "updated_date": str(self.updated_date) if self.updated_date is not None else None,
         }
@@ -61,42 +61,51 @@ class User:
         return {
             "id": self.id,
             "email": self.email,
-            "name": self.name,
+            "password": self.password,
+            "role": self.role,
+            "last_login": self.last_login,
             "token": self.token,
-            "last_login": str(self.last_login) if self.last_login is not None else None,
-            "picture": self.picture,
+            "created_date": str(self.created_date) if self.created_date is not None else None,
+            "updated_date": str(self.updated_date) if self.updated_date is not None else None,
         }
 
     def to_response_profile(self):
         return {
             "id": self.id,
-            "email": self.email,
             "name": self.name,
-            "hp": self.hp,
+            "email": self.email,
+            "password": self.password,
             "address": self.address,
-            "picture": self.picture,
             "birth_date": self.birth_date,
+            "role": self.role,
             "birth_place": self.birth_place,
-            "created_date": str(self.created_date) if self.created_date is not None else None,
-            "updated_date": str(self.updated_date) if self.updated_date is not None else None,
+            "picture": self.picture,
+            "activate": self.activate,
+            "last_login": self.last_login,
+            "hp": self.hp,
         }
 
     def to_response(self):
         return {
             "id": self.id,
-            "email": self.email,
             "name": self.name,
-            "last_login": str(self.last_login) if self.last_login is not None else None,
-            "hp": self.hp,
+            "email": self.email,
+            "password": self.password,
+            "address": self.address,
+            "birth_date": self.birth_date,
+            "role": self.role,
+            "birth_place": self.birth_place,
             "picture": self.picture,
-            "active": self.active,
+            "activate": self.activate,
+            "last_login": self.last_login,
+            "hp": self.hp,
+            "token": self.token,
             "created_date": str(self.created_date) if self.created_date is not None else None,
             "updated_date": str(self.updated_date) if self.updated_date is not None else None,
         }
 
     def to_response_forgot(self):
         return {"email": self.email}
-
 
     def to_response_simple(self):
         return {
