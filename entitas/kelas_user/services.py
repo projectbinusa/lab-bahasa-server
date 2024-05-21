bf4893fc190210dbe6d145e790bc5eaaf4416bd6
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-05-21 13:32:37 +0700

    update

--- entitas/kelas_user/services.py
+++ entitas/kelas_user/services.py
@@ -1,3 +1,6 @@
+import uuid
+
+from config.config import LOG_BOOK_FOLDER, DOMAIN_FILE_URL
 from entitas.kelas_user import repositoriesDB
 from entitas.user.services import find_user_db_by_id
 from util.other_util import raise_error
@@ -16,25 +19,35 @@ def find_kelas_user_db_by_id(id=0, to_model=False):
     return result.to_response()
 
 def find_kelas_user_for_student_by_id(id=0, user_id=0, to_model=False):
-    result = repositoriesDB.find_by_kelas_user_id_and_user_id(id=id, user_id=user_id)
+    result = repositoriesDB.find_by_id(id=id, user_id=user_id)
     if result is None:
         raise_error('data not found')
     if to_model:
         return result
-    if result.user_id != user_id:
-        raise_error('have no access')
     return result.to_response()
 
 def get_kelas_user_ids_by_user_id(user_id=0):
     return repositoriesDB.get_kelas_user_ids_by_user_id(user_id=user_id)
 
-def update_kelas_user_db(json_object={}):
+def update_kelas_user_db(json_object={}, file=None):
+    if file is None:
+        raise raise_error('File not found')
+    temp_file = str(uuid.uuid4()) + file.filename.replace(" ", "")
+    with open(LOG_BOOK_FOLDER + temp_file, "wb") as f:
+        f.write(file.file.read())
+    json_object["file"] = DOMAIN_FILE_URL + '/files/' + temp_file
     return repositoriesDB.update(json_object=json_object)
 
 def kelas_user_active_db(json_object={}):
     return repositoriesDB.class_active(json_object=json_object)
 
-def insert_kelas_user_db(json_object={}, user_id=0):
+def insert_kelas_user_db(json_object={}, file=None):
+    if file is None:
+        raise raise_error('File not found')
+    temp_file = str(uuid.uuid4()) + file.filename.replace(" ", "")
+    with open(LOG_BOOK_FOLDER + temp_file, "wb") as f:
+        f.write(file.file.read())
+    json_object["file"] = DOMAIN_FILE_URL + '/files/' + temp_file
     return repositoriesDB.insert(json_object=json_object)
 
 def delete_kelas_user_by_id(id=0):
