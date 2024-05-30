8dbc560caeb8cd1328a6a394418b80d4e98414b2
ZiyanHaidar <ziyanhaidar809@gmail.com>
2024-05-30 15:27:14 +0700

    membuat start_competition, first to answer, enter to answer, demo to answer

--- entitas/question/models.py
+++ entitas/question/models.py
@@ -14,7 +14,7 @@ class Question:
             created_date=None,
             updated_date=None
     ):
-        self.id= id
+        self.id = id
         self.think_time = think_time
         self.answer_time = answer_time
         self.class_id = class_id
@@ -55,6 +55,27 @@ class Question:
             "answer_time_client": self.answer_time_client,
             "answer": self.answer,
             "type": self.type,
+            "created_date": str(self.created_date) if self.created_date is not None else None,
+            "updated_date": str(self.updated_date) if self.updated_date is not None else None
+        }
+
+    def to_response_competition(self):
+        return {
+            "id": self.id,
+            "think_time": self.think_time,
+            "answer_time": self.answer_time,
+            "class_id": self.class_id,
+            "type": self.type,
+            "created_date": str(self.created_date) if self.created_date is not None else None,
+            "updated_date": str(self.updated_date) if self.updated_date is not None else None
+        }
+
+    def to_response_competition_answer(self):
+        return {
+            "id": self.id,
+            "class_id": self.class_id,
+            "user_id": self.user_id,
+            "answer": self.answer,
             "created_date": str(self.created_date) if self.created_date is not None else None,
             "updated_date": str(self.updated_date) if self.updated_date is not None else None
         }
