85fb67d49af72d960de1f7b641b927a5a01345de
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-06-03 08:23:04 +0700

    update

--- entitas/user/resources.py
+++ entitas/user/resources.py
@@ -320,7 +320,7 @@ class ManagementListResource:
         limit = int(req.get_param("limit", required=False, default=9))
         data, pagination = services.get_list_by_class_id(
             class_id=class_id, page=page, limit=limit, filters=filters)
-
+        resouce_response_api(resp=resp, data=data, pagination=pagination)
     # def on_post(self, req, resp, class_id):
     #     print("di resources ==> ", class_id)
     #     resouce_response_api(resp=resp,
