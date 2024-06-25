511f99aae422399507cb235463a63a80f2b2f3d7
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-06-25 08:14:09 +0700

    update

--- util/fcm_service.py
+++ util/fcm_service.py
@@ -1,16 +1,12 @@
-import requests
-import json
-from config import config
 from pyfcm import FCMNotification
+from config import config
 
 class FcmService:
-    def __init__(self):
-        self.push_service = FCMNotification(api_key=config.firebase_server_key)
+    def init(self):
+        self.push_service = FCMNotification(config.firebase_server_key, project_id=config.firebase_project_id)
 
-    def broadcast(self, topic_name='', message='',title=''):
-        self.push_service.notify_topic_subscribers(topic_name=topic_name, message_body=message,
-                                                   message_title=title)
+    def broadcast(self, topic_name='', message='', title=''):
+        self.push_service.notify_topic_subscribers(topic_name=topic_name, message_body=message, message_title=title)
 
     def send_message(self, registration_id='', message='', title=''):
-        self.push_service.notify_single_device(registration_id=registration_id, message_body=message,
-                                               message_title=title)
+        self.push_service.notify_single_device(registration_id=registration_id, message_body=message, message_title=title)
